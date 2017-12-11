0d43e0d537cb8d49b653f7e8e8bd6b005946b326
xinroom <jiayou-xin@qq.com>
2017-12-11 10:43:37 +0800

    more likely the tree

--- python/tree.py
+++ python/tree.py
@@ -1,10 +1,30 @@
 import turtle
 def tree(long,pen):
-    if long > 40:
+    if long > 30:
+        if long>100:
+            pen.pensize(5)
+        elif long<=100 and long>80:
+            pen.pensize(3)
+        else:
+            pen.pensize(2)
         if long<80:
             pen.color("green")
         pen.forward(long)
         pen.color("brown")
+        pen.forward(long/2)
+        pen.backward(long/2)
+        if long<45:
+            pen.color("#00ff3a")
+            pen.right(15)
+            pen.forward(long/3)
+            pen.left(30)
+            pen.forward(long/3)
+            pen.right(30)
+            pen.backward(long/3)
+            pen.left(30)
+            pen.backward(long/3)
+            pen.right(15)
+            pen.color("green")
         pen.right(30)
         tree(long/6*5,pen)
         pen.left(60)
